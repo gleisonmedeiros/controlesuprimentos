41ca028b08eb24fa61eb63b5cb9a20ffab3ff3a7
gleisongpm@gmail.com <@06079292Gpm>
2026-04-20 17:47:08 -0300

    corrigindo alerta

--- controlesuprimentos/cltsupri/context_processors.py
+++ controlesuprimentos/cltsupri/context_processors.py
@@ -1,11 +1,8 @@
+from django.db.models import F
 from .models import Suprimento
 
 def toners_baixos(request):
-    """
-    Injeta no contexto global de todas as views os suprimentos
-    que possuem quantidade 15 ou menor.
-    """
     if request.user.is_authenticated:
-        toners_baixo = Suprimento.objects.filter(quantidade__lte=15)
+        toners_baixo = Suprimento.objects.filter(quantidade_minima__gt=0, quantidade__lte=F('quantidade_minima'))
         return {'toners_baixo': toners_baixo}
     return {'toners_baixo': None}
